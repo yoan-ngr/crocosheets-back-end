91e735762cf19c59108a5f785017f8452269bcd9
yoan-ngr <yoan.nougueruiz@gmail.com>
2023-10-26 15:39:29 +0200

    Base de BDD SQLite

---
+++ database.js
@@ -0,0 +1,42 @@
+var sqlite3 = require('sqlite3').verbose()
+var bcrypt = require('bcrypt')
+
+const DBSOURCE = "db.sqlite"
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+        // Cannot open database
+        console.error(err.message)
+        throw err
+    }else{
+        console.log('Connected to the SQLite database.')
+
+        const saltRounds = 10;
+
+        db.run(`CREATE TABLE user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text, 
+            email text UNIQUE, 
+            password text, 
+            CONSTRAINT email_unique UNIQUE (email)
+            )`,
+            (err) => {
+                if (err) {
+                    // Table already created
+                }else{
+                    // Table just created, creating some rows
+                    var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+                    bcrypt.hash("admin123456", saltRounds, function(err, hash) {
+                        // Store hash in your password DB.
+                        db.run(insert, ["admin","admin@example.com",hash])
+                    });
+                    bcrypt.hash("user123456", saltRounds, function(err, hash) {
+                        // Store hash in your password DB.
+                        db.run(insert, ["user","user@example.com",hash])
+                    });
+                }
+            });
+    }
+});
+
+module.exports = db
--- index.js
+++ index.js
@@ -4,12 +4,127 @@ const { createServer } = require('node:http');
 // app est la fonction de rappel créée par Express
 const app = express();
 
+var db = require('./database');
+var bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
+
+app.get("/api/users", (req, res, next) => {
+    var sql = "select * from user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+    });
+});
+
+app.get("/api/user/:id", (req, res, next) => {
+    var sql = "select * from user where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+    });
+});
+
+app.post("/api/user/", (req, res, next) => {
+    var errors=[]
+    if (!req.body.password){
+        errors.push("No password specified");
+    }
+    if (!req.body.email){
+        errors.push("No email specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+
+    var saltRounds = 10;
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        var data = {
+            name: req.body.name,
+            email: req.body.email,
+            password : hash
+        }
+        var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
+        var params =[data.name, data.email, data.password]
+        db.run(sql, params, function (err, result) {
+            if (err){
+                res.status(400).json({"error": err.message})
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": data,
+                "id" : this.lastID
+            })
+        });
+    });
+
+})
+
+app.patch("/api/user/:id", (req, res, next) => {
+    var saltRounds = 10;
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        var data = {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password ? hash : null
+        }
+        db.run(
+            `UPDATE user set 
+           name = COALESCE(?,name), 
+           email = COALESCE(?,email), 
+           password = COALESCE(?,password) 
+           WHERE id = ?`,
+            [data.name, data.email, data.password, req.params.id],
+            function (err, result) {
+                if (err) {
+                    res.status(400).json({"error": res.message})
+                    return;
+                }
+                res.json({
+                    message: "success",
+                    data: data,
+                    changes: this.changes
+                })
+            });
+    });
+})
+
+app.delete("/api/user/:id", (req, res, next) => {
+    db.run(
+        'DELETE FROM user WHERE id = ?',
+        req.params.id,
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({"message":"deleted", changes: this.changes})
+        });
+})
 
 const server = createServer(app);
 const port = 3000;
 
 // lancement du serveur avec log du moment où il est prêt
-server.listen(port, () => console.log(`Server launched on port ${port}`));
+server.listen(port, () => console.log(`Serveur lancé sur http://localhost:${port}`));
